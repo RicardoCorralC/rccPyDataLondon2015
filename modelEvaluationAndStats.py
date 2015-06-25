a72e352a793ca32d52c9d27232152976874fa25b
Ricardo Corral C <ricardo@suggestic.com>
2015-06-25 14:25:44 +0200

    Random hyperparameter selection

--- modelEvaluationAndStats.py
+++ modelEvaluationAndStats.py
@@ -11,8 +11,9 @@ from sklearn import grid_search
 from collections import Counter
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.grid_search import RandomizedSearchCV
 
-def dbParser(db,level=1,dbtype='CATH',minsamples=100,verbose=False):
+def dbParser(db,level=1,dbtype='CATH',minsamples=500,verbose=False):
     sepchar = ''
     if dbtype == 'CATH':
         sepchar = '_'
@@ -60,31 +61,68 @@ def makeElenaTuning(dataset,dbtype='CATH',level=1,k_iters=3,clf = ExtraTreesClas
 
     labels = dataDict['target_names']
 
-    parameters = {"min_samples_split":[1,2,3,4,5], "max_depth":[None,1,2,3,4,5]}
+    parameters = {"min_samples_split":[1,2,3,4,5], "max_features":[None,1,2,3,4,5]}
 
 
     grid = grid_search.GridSearchCV(clf, parameters, cv= 3)
     grid.fit(dataDict['vectors'], dataDict['target_names'])
 
-    print "The best parameters are: min_samples_split=", grid.best_params_['min_samples_split'],"and max_depth=",grid.best_params_['max_depth']
+    print "The best parameters are: min_samples_split=", grid.best_params_['min_samples_split'],"and max_depth=",grid.best_params_['max_features']
 
     score_dict = grid.grid_scores_
     scores = [x[1] for x in score_dict]
-    scores = np.array(scores).reshape(len(parameters["min_samples_split"]), len(parameters["max_depth"]))
+    scores = np.array(scores).reshape(len(parameters["min_samples_split"]), len(parameters["max_features"]))
     scores = np.transpose(scores)
 
     # Make a heatmap with the performance
     #plt.figure(figsize=(12, 6))
     plt.imshow(scores, interpolation='nearest', origin='higher', cmap=plt.cm.get_cmap('jet_r'))
     plt.xticks(np.arange(len(parameters["min_samples_split"])), parameters["min_samples_split"])
-    plt.yticks(np.arange(len(parameters["max_depth"])), parameters["max_depth"])
+    plt.yticks(np.arange(len(parameters["max_features"])), parameters["max_features"])
     plt.xlabel('min_samples_split')
-    plt.ylabel('max_depth')
+    plt.ylabel('max_features')
 
     cbar = plt.colorbar()
     cbar.set_label('Classification Accuracy', rotation=270, labelpad=20)
 
     plt.show()
+
+
+def report(grid_scores, n_top=3):
+    from operator import itemgetter
+    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+    for i, score in enumerate(top_scores):
+        print("Model with rank: {0}".format(i + 1))
+        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+              score.mean_validation_score,
+              np.std(score.cv_validation_scores)))
+        print("Parameters: {0}".format(score.parameters))
+        print("")
+
+
+def makeRandomCV(dataset,dbtype='CATH',level=1,k_iters=10,clf = ExtraTreesClassifier(n_estimators=5,class_weight='auto')):
+    from scipy.stats import randint as sp_randint
+    dataDict = dbParser(dataset,level=level,dbtype=dbtype)
+    print dataDict
+
+    labels = dataDict['target_names']
+
+    param_dist = {"max_depth": [3, None],
+              "max_features": sp_randint(1, 11),
+              "min_samples_split": sp_randint(1, 11),
+              "min_samples_leaf": sp_randint(1, 11),
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+
+    n_iter_search = k_iters
+    random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
+                                   n_iter=n_iter_search)
+
+
+    random_search.fit(dataDict['vectors'], dataDict['target_names'])
+    report(random_search.grid_scores_)
+
+
 
 
 def makeAllEvals(dataset,dbtype='CATH',level=1,k_iters=10):
@@ -96,7 +134,7 @@ def makeAllEvals(dataset,dbtype='CATH',level=1,k_iters=10):
     skf = StratifiedKFold(labels, k_iters)
 
 
-    clf = ExtraTreesClassifier(n_estimators=300 ,class_weight='auto')
+    clf = ExtraTreesClassifier(n_estimators=250 ,class_weight='auto',max_depth=None,min_samples_split=4)
 
     accsList = []
 
